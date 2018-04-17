cbe399861c97dbf1ad072c031f7ac0e4c5511cfd
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-17 15:53:47 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var meals = {};
+var meals = new Object();
+
+var meals = { breakfast: 'oatmeal'};
+var meals = new Object({breakfast: 'oatmeal'});
+
+const firstMeal = 'breakfast'
+var meals = {firstMeal: 'oatmeal'}
+
+var meals = {[firstMeal]: 'oatmeal'}
+var meals = { breakfast: 'oatmeal'}
